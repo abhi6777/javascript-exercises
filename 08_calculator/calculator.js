79970dc58e5305c33578a105aa4424014a46fefc
Abhimanyu Mahto <abhimanyuverma677@gmail.com>
2024-01-21 15:01:06 +0530

    completed calculator assignments

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  return number.reduce((count, element) => count * element, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return (num1 ** num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result;
+  if(number === 0 || number === 1){
+    return 1;
+  } else {
+    result = number * factorial(number - 1);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
